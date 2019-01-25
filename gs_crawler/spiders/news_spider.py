2ec698439069c341ad6e0761213ec4a0f0a99b95
EnzoRobaina <enzo.robaina@gmail.com>
2019-01-25 10:51:08 -0200

    Adiciona arquivos iniciais do projeto

---
+++ gs_crawler/spiders/news_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+import json
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    start_urls = [
+        'https://www.campos.rj.gov.br/fcategoria.php?id_categoria=4',
+        'https://www.campos.rj.gov.br/fcategoria.php?id_categoria=4&PGpagina=2&PGporPagina=15'
+    ]
+      
+    def parse(self, response):
+        data = []
+        
+        for n in response.css('li.item-destaque'):
+            data.append({
+                'a': 'https://www.campos.rj.gov.br/' + n.css('a.title-header::attr(href)').extract_first(),
+                'thumb': n.css('img.thumbnail::attr(src)').extract_first(),
+                'titulo': n.css('b::text').extract_first(),
+                'texto': n.css('p.data-contente-destaque::text').extract_first()
+            })
+        return scrapy.Request('http://127.0.0.1:8000/postnoticias/', method='POST', body=json.dumps(data), headers={'Authorization': 'Basic ' + 'YWRtaW46YWRtaW5hZG1pbg==', 'Content-Type':'application/json'}, callback=self.post_cb)
+                                                                                                                                    
+    def post_cb(self, response):
+        print(response.status)
